ad2976ffdd244c9b27f238f4a4061cc6a5f0d5d2
DISENOMAKEMARK <disenomakemark@iMac-de-DISENOMAKEMARK.local>
2016-10-19 11:19:03 -0500

    Configuración inicial del proyecto.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
 var browserSync = require('browser-sync').create();
 
 gulp.task('default', function() {
-  // place code for your default task here
+  runSequence(['sass','browserSync', 'watch'],
+    callback
+  )
 });
 
 gulp.task('sass', function(){
@@ -15,9 +23,49 @@ gulp.task('sass', function(){
     }))
 })
 
+//Gulp useref
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    //Minify if is a javascript file
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
+
+//Gulp images optimization
+gulp.task('images', function(){
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+  // Caching images that ran through imagemin
+  .pipe(cache(imagemin({
+      interlaced: true
+    })))
+  .pipe(gulp.dest('dist/images'))
+});
+
+//Gulp fonts optimization
+gulp.task('fonts', function() {
+  return gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+})
+
+//Gulp delete unnecesary files and folders
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+
 //Gulp watch syntax
 gulp.task('watch',['browserSync', 'sass'],function(){
   gulp.watch('app/sass/**/*.sass', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+})
+
+//Gulp build syntax
+gulp.task('build', function (callback) {
+  runSequence('clean:dist',
+    ['sass', 'useref', 'images', 'fonts'],
+    callback
+  )
 })
 
 gulp.task('browserSync', function() {
