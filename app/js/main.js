3b26e6748793cb9f276ff685dd860fc32e711496
DISENOMAKEMARK <disenomakemark@iMac-de-DISENOMAKEMARK.local>
2016-10-24 17:53:51 -0500

    Full slider and Full navigation approach.

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,130 @@
-$( "#nav-open" ).click(function() {
-  $( "#full-menu" ).slideDown( "slow", function() {
-    // Animation complete.
+$(document).ready(function() {
+  $( "#nav-open" ).click(function() {
+    $( "#full-menu" ).toggleClass( "open-menu");
   });
-});
 
-$( "#service-thumb--link" ).mouseover(function() {
-  $( "#graph-block" ).fadeOut( "slow", function() {
-    // Animation complete.
+  $( "#service-thumb--link" ).mouseover(function() {
+    $( "#graph-block" ).fadeOut( "slow", function() {
+      // Animation complete.
+    });
   });
+
+  var clock = (function(){
+    'use-strict';
+    return {
+        // new date
+        now: new Date(),
+        // short selector
+        _: function(e){
+            return document.querySelector(e);
+        },
+        // check time
+        checkTime: function(a){
+            return (((a < 10) ? "0" : "") + a);
+        },
+        // show date
+        showDate: function(){
+            // if exists
+            if (this._("#date")) {
+                // days
+                var days = [
+                    'Domingo', 'Lunes',
+                    'Martes', 'Miércoles',
+                    'Jueves', 'Viernes',
+                    'Sábado'
+                ];
+                // months
+                var months = [
+                    'Enero', 'Febrero', 'Marzo',
+                    'Abril', 'Mayo', 'Junio',
+                    'Julio', 'Agosto', 'Septiembre',
+                    'Octubre', 'Noviembre', 'Diciembre'
+                ];
+                // show html
+                this._("#date").innerHTML =
+                    days[this.now.getDay()] + ", " +
+                    this.checkTime(this.now.getDate()) + " de " +
+                    months[this.now.getMonth()] + " de " +
+                    this.now.getFullYear();
+            }
+        },
+        // init clock and date
+        init: function(){
+            var self = this;
+            setInterval(this.newClock,1000);
+            this.showDate();
+        }
+    };
+})();
+clock.init();
+
+
+  $('#fullpage').fullpage({
+      //Navigation
+      menu: '#menu',
+      lockAnchors: false,
+      anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+      navigation: false,
+      navigationPosition: 'right',
+      navigationTooltips: ['firstSlide', 'secondSlide'],
+      showActiveTooltip: true,
+      slidesNavigation: true,
+      slidesNavPosition: 'bottom',
+
+      //Scrolling
+      css3: true,
+      scrollingSpeed: 700,
+      autoScrolling: true,
+      fitToSection: true,
+      fitToSectionDelay: 1000,
+      scrollBar: false,
+      easing: 'easeInOutCubic',
+      easingcss3: 'ease',
+      loopBottom: false,
+      loopTop: false,
+      loopHorizontal: true,
+      continuousVertical: false,
+      continuousHorizontal: false,
+      scrollHorizontally: false,
+      interlockedSlides: false,
+      resetSliders: false,
+      fadingEffect: false,
+      normalScrollElements: '#element1, .element2',
+      scrollOverflow: false,
+      scrollOverflowOptions: null,
+      touchSensitivity: 15,
+      normalScrollElementTouchThreshold: 5,
+      bigSectionsDestination: null,
+
+      //Accessibility
+      keyboardScrolling: true,
+      animateAnchor: true,
+      recordHistory: true,
+
+      //Design
+      controlArrows: true,
+      verticalCentered: true,
+      sectionsColor : ['#ccc', '#fff'],
+      paddingTop: '3em',
+      paddingBottom: '10px',
+      fixedElements: '#header, .footer',
+      responsiveWidth: 0,
+      responsiveHeight: 0,
+      responsiveSlides: false,
+
+      //Custom selectors
+      sectionSelector: '.section',
+      slideSelector: '.slide',
+
+      //events
+      onLeave: function(index, nextIndex, direction){},
+      afterLoad: function(anchorLink, index){},
+      afterRender: function(){},
+      afterResize: function(){},
+      afterResponsive: function(isResponsive){},
+      afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+      onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+  });
+
+
 });
