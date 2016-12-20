593a7270240318615d2c4a6e86f1cb99a5465cfb
Web MakeMark <webmakemark@n175-33-0-56.meb2.vic.optusnet.com.au>
2016-12-20 10:59:33 -0500

    animacion input terminada

---
+++ app/js/animacion-input.js
@@ -0,0 +1,24 @@
+$(function() {
+   
+  var texto = $('.animar-texto').html();
+  var partir = texto.split("");
+  $('.animar-texto').html("");
+  for (var i = 0; i < partir.length; i++) {
+    $('.animar-texto').append('<span class="letras">'+ partir[i] +'</span>');
+  }
+
+  $('.letras').each(function(index) {
+    var randX = Math.floor(Math.random() * $(window).width());
+    var randY = Math.floor(Math.random() * $(window).height());
+
+    if (index >= 0) {
+      $(this).css({"left":randX+'px', "top":randY+'px', "opacity":0}, 3000 );
+    }
+  });
+
+  $('.animar-texto-h').mouseenter(function() {
+    $('.letras').animate({"left": 0, "top": 0, "opacity": 1}, 3000);
+  });
+ 
+
+});
